0fb324e6b78b143c8c16658dc4f13cf1de88ded0
Luca Vizzarro <2252593v@student.gla.ac.uk>
2018-03-22 00:47:10 +0000

    admin static files

--- app/settings.py
+++ app/settings.py
@@ -19,8 +19,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS=[STATIC_DIR,]
-
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
